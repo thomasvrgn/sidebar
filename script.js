c8ee18da58e14f4edd85738eec1ac68737a9971b
NessMC <contact@nessmc.fr>
2020-07-31 16:34:45 +0200

    🐛 Button children interaction added.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const navbar = document.getElementsByTagName('nav'),
   button = document.getElementsByClassName('sidebar-button')[0]
 
-console.log(button)
+function checkParent (parent, child) { 
+  let node = child.parentNode
+  while (node != null) { 
+    if (node == parent) { 
+      return true
+    } 
+    node = node.parentNode
+  } 
+  return false
+} 
 
 window.addEventListener('load', function () {
   navbar[0].style.left = -navbar[0].clientWidth + 'px'
@@ -9,4 +18,14 @@ window.addEventListener('load', function () {
 
 button.addEventListener('click', function () {
   navbar[0].style.left = '0px'
+})
+
+window.addEventListener('click', function (event) {
+  if (event.target !== button) {
+    if (event.target !== navbar && !checkParent(button, event.target)) {
+      navbar[0].style.left = -navbar[0].clientWidth + 'px'
+    }
+  } else {
+    navbar[0].style.left = '0px'
+  }
 })
